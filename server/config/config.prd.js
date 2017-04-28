bb711ada5380bc99bfde00cd896f4e1e1106e897
Goran Jovanov <goran.jovanov@gmail.com>
2017-04-28 17:12:40 +0200

    Mongodb ports update

--- server/config/config.prd.js
+++ server/config/config.prd.js
@@ -1,5 +1,5 @@
 var config = {};
 config.env = "prd";
 config.httpsPort = 444;
-config.xplorifyDb = "mongodb://localhost/xplorifydb";
+config.xplorifyDb = "mongodb://localhost:1212/xplorifydb";
 module.exports = config;
--- server/config/config.test.js
+++ server/config/config.test.js
@@ -1,5 +1,5 @@
 var config = {};
 config.env = "test";
 config.httpsPort = 444;
-config.xplorifyDb = "mongodb://localhost/xplorifydb";
+config.xplorifyDb = "mongodb://localhost:1212/xplorifydb";
 module.exports = config;
