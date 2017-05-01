fcb9625c0921f9f9e52e4b48b21f754b383ad013
Goran Jovanov <goran.jovanov@gmail.com>
2017-05-01 20:42:43 +0200

    Dev config updated

--- server/config/config.dev.js
+++ server/config/config.dev.js
@@ -1,6 +1,6 @@
 var config = {};
 config.env = "dev";
 config.httpsPort = 444;
-// config.xplorifyDb = "mongodb://gjovanov:Xc0nnected@live.xplorify.net:1212/xplorifydb";
+// config.xplorifyDb = "mongodb://live.xplorify.net:1212/xplorifydb";
 config.xplorifyDb = "mongodb://localhost/xplorifydb";
 module.exports = config;
