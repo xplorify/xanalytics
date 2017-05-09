cbbf45ca8898f6f32fbce985ecd3c2c946ec5b66
Violeta Ivkovska <violeta.ivkovska@inovatika.net>
2017-05-09 13:12:58 +0200

    Ip Address Fixes

--- server/analytics/api/ip-controller.js
+++ server/analytics/api/ip-controller.js
@@ -3,12 +3,18 @@
 var ipService = require("../services/ip/ip-service");
 
 module.exports = function (app, config) {
-    app.get("/getGlobals", function (req, res) {
+    app.get("/getGlobals", function (request, res) {
         res.header("Content-Type", "application/json");
-        var ip = req.headers['x-forwarded-for'] ||
-            req.connection.remoteAddress ||
-            req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress;
+        var ip = request.headers['x-forwarded-for'] ||
+            request.connection.remoteAddress ||
+            request.socket.remoteAddress ||
+            request.connection.socket.remoteAddress;
+        ip = ip.split(',')[0];
+        ip = ip.split(':').slice(-1); //in case the ip returned in a format: "::ffff:146.xxx.xxx.xxx"
+        if (ip == "1") {
+            ip = "95.86.38.12";
+        }
+
         return ipService.getGeoIpInfo(ip)
             .then(function (result) {
                 console.log("Ip Controller: " + result);
