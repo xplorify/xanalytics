195d1b0f4cf6e9ea3bd8b3c2113a8d40e6d938e9
Violeta Ivkovska <violeta.ivkovska@inovatika.net>
2017-05-09 12:16:57 +0200

    Console logs added

--- server/analytics/api/ip-controller.js
+++ server/analytics/api/ip-controller.js
@@ -4,16 +4,19 @@ var ipService = require("../services/ip/ip-service");
 
 module.exports = function (app, config) {
     app.get("/getGlobals", function (req, res) {
+        res.header("Content-Type", "application/json");
         var ip = req.headers['x-forwarded-for'] ||
             req.connection.remoteAddress ||
             req.socket.remoteAddress ||
             req.connection.socket.remoteAddress;
         return ipService.getGeoIpInfo(ip)
             .then(function (result) {
-                res.send({ result:  result });
+                console.log("Ip Controller: " + result);
+                res.send({ result: result });
             })
             .catch(function (err) {
                 res.status(500);
+                console.log("Ip Controller catch: " + err);
                 res.send({ error: err });
             });
     });
--- server/analytics/services/ip/ip-service.js
+++ server/analytics/services/ip/ip-service.js
@@ -8,11 +8,13 @@ ipService.getGeoIpInfo = function (ipAddress) {
     if (ipAddress == "::1") {
         ipAddress = "95.86.38.12";
     }
+    console.log('IP address: ' + ipAddress);
     return fetch(config.freeGeoIpUrl + "/" + ipAddress, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
     })
         .then(function (res) {
+            console.log('FreeGeopIP result: ' + res.json());
             return res.json();
         });
 }
