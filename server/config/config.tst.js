32db10924a23067d1b1cd53e225dd87417b32cfe
Violeta Ivkovska <violeta.ivkovska@inovatika.net>
2017-07-06 12:38:05 +0200

    free geo ip url changed

--- server/config/config.tst.js
+++ server/config/config.tst.js
@@ -4,6 +4,6 @@ config.httpsPort = 444;
 config.jwtSecret = "pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L";
 config.xplorifyDb = "mongodb://XplorifyAdmin:Xp12345!@live.xplorify.net:1212/xplorifydb";
 config.serverUrl = "https://live.xplorify.net";
-config.freeGeoIpUrl = "http://138.201.48.14/json";
+config.freeGeoIpUrl = "http://geo.xplorify.net/json";
 config.application = { code: "blog", url: "https://blog.xplorify.mk" };
 module.exports = config;
