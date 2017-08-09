35d126953881e53545b37c894ea503f79281f758
Violeta Ivkovska <violeta.ivkovska@inovatika.net>
2017-08-09 13:39:34 +0200

    url bug fix

--- server/config/config.prd.js
+++ server/config/config.prd.js
@@ -5,6 +5,7 @@ config.jwtSecret = "pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91c
 config.xplorifyDb = "mongodb://XplorifyAdmin:Xp12345!@live.xplorify.net:1212/xplorifydb";
 config.serverUrl = "https://analytics.xplorify.net";
 config.freeGeoIpUrl = "http://geo.xplorify.net/json";
+config.analyticsUrl = "http://analytics.xplorify.net/analytics";
 config.sendGridKey = "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";
 config.application = { code: "blog", url: "https://blog.xplorify.mk" };
 module.exports = config;
