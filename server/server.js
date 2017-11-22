1c0243b2ac0bcf2afecf99cb403d9f50bd42c422
Atanas Lazarovski <atanas.lazarovski@inovatika.net>
2017-11-22 14:52:03 +0100

    Cache Controll server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-  res.header("Access-Control-Allow-Headers", "origin, content-type, Authorization");
+  res.header("Access-Control-Allow-Headers", "origin, content-type, Authorization, Cache-Control");
   res.header("Content-Type", "application/json");
   if (req.method === "OPTIONS") {
     res.send(200);
