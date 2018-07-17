2620fed7ad7eeacc1003bdcc3f707e4953464390
Atanas Lazarovski <atanas.lazarovski@inovatika.net>
2018-07-17 16:59:09 +0200

    dockerfile cange branch

--- dashboard2/app/config/config.prd.js
+++ dashboard2/app/config/config.prd.js
@@ -2,6 +2,6 @@ export let config = {
   settings: {
     domainUrl: "https://live.xplorify.net:445",
     janusServerUrl: ["https://hello.xplorify.net:8089/janus"],
-    analyticsUrl: "https://analytics.xplorify.net/analytics?r"
+    analyticsUrl: "http://analytics.met-pro.net/analytics?r"
   }
 };
