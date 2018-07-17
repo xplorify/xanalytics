35102da84ce581837c9a05a566c8df2a55e4c3b9
Atanas Lazarovski <atanas.lazarovski@inovatika.net>
2018-07-17 11:09:52 +0200

    fonts

--- dashboard2/app/init.js
+++ dashboard2/app/init.js
@@ -1,4 +1,4 @@
-import 'font-awesome/css/font-awesome.css';
+// import 'font-awesome/css/font-awesome.css';
 import 'w3-css/w3.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css';
